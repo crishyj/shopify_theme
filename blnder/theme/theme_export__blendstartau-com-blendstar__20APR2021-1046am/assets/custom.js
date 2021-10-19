5d1072f1371778961cdd649fc66d444eadcc87fb
crishyj <ilonka.melochek01@gmail.com>
2021-10-19 00:18:55 -0700

    first commit

---
+++ blnder/theme/theme_export__blendstartau-com-blendstar__20APR2021-1046am/assets/custom.js
@@ -0,0 +1,41 @@
+
+jQuery(function(){
+
+  jQuery("#contactFormName")
+    .on("focus", function(){
+          $('.input-span1').css('transform', 'none');
+    })
+    .on("blur", function(){
+         $('.input-span1').css('transform', 'scaleX(0)');
+    });
+  
+  jQuery("#contactFormEmail")
+    .on("focus", function(){
+          $('.input-span2').css('transform', 'none');
+    })
+    .on("blur", function(){
+         $('.input-span2').css('transform', 'scaleX(0)');
+    });
+    
+  jQuery("#contactFormTelephone")
+    .on("focus", function(){
+          $('.input-span3').css('transform', 'none');
+    })
+    .on("blur", function(){
+         $('.input-span3').css('transform', 'scaleX(0)');
+    });
+    
+  jQuery("#contactFormMessage")
+    .on("focus", function(){
+          $('.input-span4').css('transform', 'none');
+    })
+    .on("blur", function(){
+         $('.input-span4').css('transform', 'scaleX(0)');
+    });
+  
+  $('.product--sale').html(''); 
+
+   
+  
+});
+
